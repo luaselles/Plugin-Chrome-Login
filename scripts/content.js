7da27d452c8c5b18d69d8974d0a0e07ed68f89db
luaselles <lu.selles@unoeste.edu.br>
2022-09-14 14:30:45 -0300

    resolve dados commit

--- scripts/content.js
+++ scripts/content.js
@@ -5,11 +5,11 @@ if(window.location.pathname == '/site/AVA/default.aspx')
         const usua = document.getElementById('tbLogin');
         const pass = document.getElementById('tbSenha'); 
         if (usua != undefined && pass != undefined) {
-            usua.setAttribute('value','262228246');
+            usua.setAttribute('value','usuario'); //no espaço usuario colocar de acordo com seu usuario de login.
             usua.dispatchEvent(new Event("change", {bubbles: true}));
             usua.dispatchEvent(new Event("blur", {bubbles: true}));
         
-            pass.setAttribute('value','18092002')
+            pass.setAttribute('value','senha'); //no espaço senha colocar de acordo com sua senha de usuario.
             pass.dispatchEvent(new Event("change", {bubbles: true}));
             pass.dispatchEvent(new Event("blur", {bubbles: true}));
             
